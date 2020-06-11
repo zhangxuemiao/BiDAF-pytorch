4ccd575a3093eae1cd7e35d3d3e13f2401af52df
XuemiaoMT <914136910@qq.com>
2020-06-11 23:17:54 +0800

    MOD: 简化

--- model/model.py
+++ model/model.py
@@ -2,7 +2,71 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from utils.nn import LSTM, Linear
+
+class LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, batch_first=False, num_layers=1, bidirectional=False, dropout=0.2):
+        super(LSTM, self).__init__()
+
+        self.rnn = nn.LSTM(input_size=input_size,
+                           hidden_size=hidden_size,
+                           num_layers=num_layers,
+                           bidirectional=bidirectional,
+                           batch_first=batch_first)
+        self.reset_params()
+        self.dropout = nn.Dropout(p=dropout)
+
+    def reset_params(self):
+        for i in range(self.rnn.num_layers):
+            nn.init.orthogonal_(getattr(self.rnn, f'weight_hh_l{i}'))
+            nn.init.kaiming_normal_(getattr(self.rnn, f'weight_ih_l{i}'))
+            nn.init.constant_(getattr(self.rnn, f'bias_hh_l{i}'), val=0)
+            nn.init.constant_(getattr(self.rnn, f'bias_ih_l{i}'), val=0)
+            getattr(self.rnn, f'bias_hh_l{i}').chunk(4)[1].fill_(1)
+
+            if self.rnn.bidirectional:
+                nn.init.orthogonal_(getattr(self.rnn, f'weight_hh_l{i}_reverse'))
+                nn.init.kaiming_normal_(getattr(self.rnn, f'weight_ih_l{i}_reverse'))
+                nn.init.constant_(getattr(self.rnn, f'bias_hh_l{i}_reverse'), val=0)
+                nn.init.constant_(getattr(self.rnn, f'bias_ih_l{i}_reverse'), val=0)
+                getattr(self.rnn, f'bias_hh_l{i}_reverse').chunk(4)[1].fill_(1)
+
+    def forward(self, x):
+        x, x_len = x
+        x = self.dropout(x)
+
+        x_len_sorted, x_idx = torch.sort(x_len, descending=True)
+        x_sorted = x.index_select(dim=0, index=x_idx)
+        _, x_ori_idx = torch.sort(x_idx)
+
+        x_packed = nn.utils.rnn.pack_padded_sequence(x_sorted, x_len_sorted, batch_first=True)
+        x_packed, (h, c) = self.rnn(x_packed)
+
+        x = nn.utils.rnn.pad_packed_sequence(x_packed, batch_first=True)[0]
+        x = x.index_select(dim=0, index=x_ori_idx)
+        h = h.permute(1, 0, 2).contiguous().view(-1, h.size(0) * h.size(2)).squeeze()
+        h = h.index_select(dim=0, index=x_ori_idx)
+
+        return x, h
+
+
+class Linear(nn.Module):
+    def __init__(self, in_features, out_features, dropout=0.0):
+        super(Linear, self).__init__()
+
+        self.linear = nn.Linear(in_features=in_features, out_features=out_features)
+        if dropout > 0:
+            self.dropout = nn.Dropout(p=dropout)
+        self.reset_params()
+
+    def reset_params(self):
+        nn.init.kaiming_normal_(self.linear.weight)
+        nn.init.constant_(self.linear.bias, 0)
+
+    def forward(self, x):
+        if hasattr(self, 'dropout'):
+            x = self.dropout(x)
+        x = self.linear(x)
+        return x
 
 
 class BiDAF(nn.Module):
@@ -17,7 +81,7 @@ class BiDAF(nn.Module):
         self.char_conv = nn.Sequential(
             nn.Conv2d(1, args.char_channel_size, (args.char_dim, args.char_channel_width)),
             nn.ReLU()
-            )
+        )
 
         # 2. Word Embedding Layer
         # initialize word embedding with GloVe
@@ -120,18 +184,18 @@ class BiDAF(nn.Module):
             q_len = q.size(1)
 
             # (batch, c_len, q_len, hidden_size * 2)
-            #c_tiled = c.unsqueeze(2).expand(-1, -1, q_len, -1)
+            # c_tiled = c.unsqueeze(2).expand(-1, -1, q_len, -1)
             # (batch, c_len, q_len, hidden_size * 2)
-            #q_tiled = q.unsqueeze(1).expand(-1, c_len, -1, -1)
+            # q_tiled = q.unsqueeze(1).expand(-1, c_len, -1, -1)
             # (batch, c_len, q_len, hidden_size * 2)
-            #cq_tiled = c_tiled * q_tiled
-            #cq_tiled = c.unsqueeze(2).expand(-1, -1, q_len, -1) * q.unsqueeze(1).expand(-1, c_len, -1, -1)
+            # cq_tiled = c_tiled * q_tiled
+            # cq_tiled = c.unsqueeze(2).expand(-1, -1, q_len, -1) * q.unsqueeze(1).expand(-1, c_len, -1, -1)
 
             cq = []
             for i in range(q_len):
-                #(batch, 1, hidden_size * 2)
+                # (batch, 1, hidden_size * 2)
                 qi = q.select(1, i).unsqueeze(1)
-                #(batch, c_len, 1)
+                # (batch, c_len, 1)
                 ci = self.att_weight_cq(c * qi).squeeze()
                 cq.append(ci)
             # (batch, c_len, q_len)
--- run.py
+++ run.py
@@ -8,8 +8,24 @@ from time import gmtime, strftime
 
 from model.model import BiDAF
 from model.data import SQuAD
-from model.ema import EMA
 import evaluate
+
+
+class EMA():
+    def __init__(self, mu):
+        self.mu = mu
+        self.shadow = {}
+
+    def register(self, name, val):
+        self.shadow[name] = val.clone()
+
+    def get(self, name):
+        return self.shadow[name]
+
+    def update(self, name, x):
+        assert name in self.shadow
+        new_average = (1.0 - self.mu) * x + self.mu * self.shadow[name]
+        self.shadow[name] = new_average.clone()
 
 
 def train(args, data):
